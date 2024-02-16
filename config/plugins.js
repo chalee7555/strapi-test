2ac6043decfd91a03a6d5ec3444891d137bec78a
arthit sokhampha <arthit.scs57@ubru.ac.th>
2024-02-16 18:09:42 +0700

    test2

--- config/plugins.js
+++ config/plugins.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
       config: {
         deployHook:
           "https://api.vercel.com/v1/integrations/deploy/prj_<deploy-hook>",
-        apiToken: "prj_sdgTkmCcDvnknbSya6uVPAXzYdR7",
+        apiToken: "WTmXl489iL0z4MSSPAjjP9Up",
         appFilter: "strapi-test",
         teamFilter: "your-team-id-on-vercel",
         roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
